f36c9f6bbcac428e8d22ee4e862fec63e8644679
Cristovao <cristovaofabio3@gmail.com>
2022-05-12 10:35:50 -0300

    css settings

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App() {
     return (
-        <div className="App">
-            <PomodoroTimer defaultPomodoroTime = {1400}/>
+        <div className="container">
+            <PomodoroTimer
+                pomodoroTime={1400}
+                shortRestTime={300}
+                longRestTime={900}
+                cycles={4}
+            />
         </div>
     );
 }
--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -4,11 +4,14 @@ import { Button } from "./button";
 import { Timer } from "./timer";
 
 interface Props {
-    defaultPomodoroTime: number;
+    pomodoroTime: number;
+    shortRestTime: number;
+    longRestTime: number;
+    cycles: number;
 }
 
 export function PomodoroTimer(props: Props): JSX.Element {
-    const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
+    const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
 
     useInterval(() => {
         setMainTime(mainTime - 1);
@@ -18,7 +21,18 @@ export function PomodoroTimer(props: Props): JSX.Element {
         <div className="pomodoro">
             <h2>Yeah!!!You are working!</h2>
             <Timer mainTime={mainTime}></Timer>
-            <Button text="Test"></Button>
+
+            <div className="controls">
+                <Button text="Test"></Button>
+                <Button text="Test"></Button>
+                <Button text="Test"></Button>
+            </div>
+
+            <div className="details">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed nemo, velit quo, blanditiis cupiditate rem optio voluptas neque quia minima totam placeat a quibusdam fugiat tempora obcaecati. Veritatis, at dicta.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed nemo, velit quo, blanditiis cupiditate rem optio voluptas neque quia minima totam placeat a quibusdam fugiat tempora obcaecati. Veritatis, at dicta.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed nemo, velit quo, blanditiis cupiditate rem optio voluptas neque quia minima totam placeat a quibusdam fugiat tempora obcaecati. Veritatis, at dicta.</p>
+            </div>
         </div>
     );
 }
